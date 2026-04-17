ee87ed5f5a075595bfdcec6f0ae8d8fd442a0a88
Guiller Altamira <altamiranoguillermo110@gmail.com>
2026-04-17 16:57:23 -0400

    restricciones en AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,9 +9,84 @@ public class AppDbContext : DbContext
     {
     }
 
-    public DbSet<Cliente> Clientes => Set<Cliente>();
-    public DbSet<Producto> Productos => Set<Producto>();
-    public DbSet<Lote> Lotes => Set<Lote>();
-    public DbSet<Venta> Ventas => Set<Venta>();
-    public DbSet<DetalleVenta> DetalleVentas => Set<DetalleVenta>();
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Lote> Lotes { get; set; }
+    public DbSet<Venta> Ventas { get; set; }
+    public DbSet<DetalleVenta> DetalleVentas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Cliente>()
+            .ToTable("Cliente");
+
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.Nombre)
+            .HasMaxLength(50);
+
+        modelBuilder.Entity<Cliente>()
+            .Property(c => c.Extension)
+            .HasMaxLength(5);
+
+
+
+        modelBuilder.Entity<Producto>()
+            .ToTable("Producto");
+
+        modelBuilder.Entity<Producto>()
+            .Property(p => p.Nombre)
+            .HasMaxLength(50);
+
+        modelBuilder.Entity<Producto>()
+            .Property(p => p.Tipo)
+            .HasMaxLength(30);
+
+        modelBuilder.Entity<Producto>()
+            .Property(p => p.Precio)
+            .HasColumnType("decimal(10,2)");
+
+
+
+        modelBuilder.Entity<Lote>()
+            .ToTable("Lote");
+
+        modelBuilder.Entity<Lote>()
+            .Property(l => l.Codigo)
+            .HasMaxLength(20);
+
+        modelBuilder.Entity<Lote>()
+            .HasOne(l => l.Producto)
+            .WithMany(p => p.Lotes)
+            .HasForeignKey(l => l.ProductoId);
+
+
+
+        modelBuilder.Entity<Venta>()
+            .ToTable("Venta");
+
+        modelBuilder.Entity<Venta>()
+            .Property(v => v.Total)
+            .HasColumnType("decimal(10,2)");
+
+
+
+        modelBuilder.Entity<DetalleVenta>()
+            .ToTable("DetalleVenta");
+
+        modelBuilder.Entity<DetalleVenta>()
+            .Property(d => d.PrecioUnitario)
+            .HasColumnType("decimal(10,2)");
+
+        modelBuilder.Entity<DetalleVenta>()
+            .Property(d => d.Subtotal)
+            .HasColumnType("decimal(10,2)");
+
+        modelBuilder.Entity<DetalleVenta>()
+            .HasOne(d => d.Venta)
+            .WithMany(v => v.Detalles)
+            .HasForeignKey(d => d.VentaId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
